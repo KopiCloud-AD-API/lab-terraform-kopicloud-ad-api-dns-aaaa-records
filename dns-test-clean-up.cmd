bbdc307cf600743e771e07a4a034d376aa6a5c76
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-05-14 12:09:42 +0200

    Initial commit

--- dns-test-clean-up.cmd
+++ dns-test-clean-up.cmd
@@ -7,31 +7,29 @@ echo -----------------------------
 
 echo DNS AAAA List #1
 del /q "dns-aaaa-records-list-1\.terraform.lock.hcl"
-del /q "dns-aaaa-records-list-1\terraform.tfstate"
+del /q "dns-aaaa-records-list-1\terraform.tfstate.*"
 rmdir /s /q "dns-aaaa-records-list-1\.terraform"
 
 echo DNS AAAA List #2
 del /q "dns-aaaa-records-list-2\.terraform.lock.hcl"
-del /q "dns-aaaa-records-list-2\terraform.tfstate"
+del /q "dns-aaaa-records-list-2\terraform.tfstate.*"
 rmdir /s /q "dns-aaaa-records-list-2\.terraform"
 
 echo DNS AAAA List #3
 del /q "dns-aaaa-records-list-3\.terraform.lock.hcl"
-del /q "dns-aaaa-records-list-3\terraform.tfstate"
-del /q "dns-aaaa-records-list-3\terraform.tfstate.backup"
+del /q "dns-aaaa-records-list-3\terraform.tfstate.*"
 rmdir /s /q "dns-aaaa-records-list-3\.terraform"
 
 echo DNS AAAA List #4
 del /q "dns-aaaa-records-list-4\.terraform.lock.hcl"
-del /q "dns-aaaa-records-list-4\terraform.tfstate"
-del /q "dns-aaaa-records-list-4\terraform.tfstate.backup"
+del /q "dns-aaaa-records-list-4\terraform.tfstate.*"
 rmdir /s /q "dns-aaaa-records-list-4\.terraform"
 
 echo DNS AAAA Create
 del /q "dns-aaaa-records-create\.terraform.lock.hcl"
-del /q "dns-aaaa-records-create\terraform.tfstate"
-del /q "dns-aaaa-records-create\terraform.tfstate.backup"
+del /q "dns-aaaa-records-create\terraform.tfstate.*"
 rmdir /s /q "dns-aaaa-records-create\.terraform"
 
 echo -----------------------------
 
+pause
